afdcd0ec61044988fd8137b50818d7ce60b05f99
Luigi <grigio@MacBook-Air-di-Luigi.local>
2015-01-13 00:43:56 +0100

    updated to rust 1.0.0-dev

--- rust_counter_atomics.rs
+++ rust_counter_atomics.rs
@@ -1,24 +1,26 @@
 // Ported to rust 0.13.0-nightly
 use std::sync::Arc;
-use std::sync::atomic::{AtomicUint, Relaxed};
+use std::sync::atomic::AtomicUint;
+use std::sync::atomic::Ordering::Relaxed;
+use std::sync::mpsc::channel;
 use std::thread::Thread;
 
-const NUM_THREADS: uint = 20;
-const NUM_INCREMENTS: uint = 1000000u;
+const NUM_THREADS: usize = 20;
+const NUM_INCREMENTS: usize = 10000000us;
 
 fn main() {
   let counter = Arc::new(AtomicUint::new(0));
   let (tx, rx) = channel();
-  for _ in range(0u, NUM_THREADS) {
+  for _ in range(0us, NUM_THREADS) {
     let (counter, tx) = (counter.clone(), tx.clone());
     Thread::spawn(move || {
-      for _ in range(0u, NUM_INCREMENTS) {
+      for _ in range(0us, NUM_INCREMENTS) {
         counter.fetch_add(1, Relaxed);
       }
       tx.send(());
-    }).detach();
+    });
   }
   // Wait for threads to finish
-  for _ in range(0u, NUM_THREADS) { rx.recv(); }
+  for _ in range(0us, NUM_THREADS) { rx.recv(); }
   println!("{}" , counter.load(Relaxed));
 }
